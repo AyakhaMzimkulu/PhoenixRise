8290132d9ab917ca21c3b5a8773303f2040a63d8
Duduzile January <duduzile.january@younglings.africa>
2022-08-17 09:38:00 +0200

    Changed name from "Otto" to "Drake" . in order to test if code runs.

--- Minions/Minions_sytem_speech.py
+++ Minions/Minions_sytem_speech.py
@@ -1,6 +1,6 @@
-import pyttsx3 #pip install pyttsx3 == text data into speech
+import pyttsx3  # pip install pyttsx3 == text data into speech
 
 engine = pyttsx3.init()
 
-engine.say('Hello, this is Otto, How may I be of assistance?')
+engine.say('Hello, this is Drake, How may I be of assistance?')
 engine.runAndWait()
